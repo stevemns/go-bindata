56fc0257e0e91c43c5c9d65bb46db40de55cd402
Steve Malmskog <stevem@netskope.com>
2019-07-02 09:36:46 -0700

    Fix minor build issues and update version output
    Provide output to indicate that this is a modified go-bindata
    binary.
    Build a static binary with the AppVersionRev variable set.
    Update gitignore with .swp files.
    Rebuild the go.mod/go.sum and correct previous depends.

--- go-bindata/version.go
+++ go-bindata/version.go
@@ -26,6 +26,6 @@ func Version() string {
 		AppVersionRev = "0"
 	}
 
-	return fmt.Sprintf("%s %d.%d.%s (Go runtime %s).\nCopyright (c) 2010-2013, Jim Teeuwen.",
+	return fmt.Sprintf("%s %d.%d.%s (Go runtime %s).\nCopyright (c) 2010-2013, Jim Teeuwen.\nIncludes updates by stevemns.",
 		AppName, AppVersionMajor, AppVersionMinor, AppVersionRev, runtime.Version())
 }
